66c0469a15ea39731ea2b31d730d9c07a193b4da
tmarti29 <tmarti29@student.fitchburgstate.edu>
2024-12-10 19:45:39 -0500

    a

--- Game Final/Assets/Scripts/GameManager.cs
+++ Game Final/Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,9 +13,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(getInput.keycode "D")
-        {
-            transform.Translate(Vector3.right);
-        }
+        
     }
 }
